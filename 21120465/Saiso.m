cd453ca3c64a190fc5bc28dc6be0e3894e0e6719
dhuu2003 <101379737+dhuu2003@users.noreply.github.com>
2023-05-31 23:01:22 +0700

    o

--- 21120465/Saiso.m
+++
@@ -1,4 +0,0 @@
-function [rEq, aEq] = saiso(q_a, q_e)
-    aEq = abs(q_a - q_e);
-    rEq = abs((q_a - q_e)/q_a);
-end
--- 21120465/simpson_13.m
+++
@@ -1,8 +0,0 @@
-function [I] = simpson_13(xx, yy)
-    n = length(xx);
-    I = 0;
-    for i = 1:2:n-2
-        I = I + (xx(i+2) - xx(i))*(yy(i+2) + 4*yy(i+1) + yy(i))/6;
-    end
-    I = double(I);
-end
